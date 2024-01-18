63be336e0ce1e7192a8445c11fe15447e249b5a6
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-01-18 23:55:02 +0300

    Created config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, './dist/'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./public/index.html")
+        }),
+        new MiniCssExtractPlugin()
+],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            },
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin],
+    },
+    devServer: {
+        open: true,
+        hot: true,
+        host: 'localhost',
+    }
+};
