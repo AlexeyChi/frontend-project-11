99a59799f30a629e93518c9548a4f9fbaaf240e0
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-01-24 22:03:25 +0300

    Complited second step

--- src/app.js
+++ src/app.js
@@ -2,52 +2,49 @@ import * as yup from 'yup';
 import watcher from './view.js';
 
 const validate = (url, feeds) => {
-    const urlShema = yup.string()
-      .url('invalid URL')
-      .notOneOf(feeds, 'not uniq URL');
+  const urlShema = yup
+    .string()
+    .url('invalid URL')
+    .notOneOf(feeds, 'not uniq URL');
 
-    return urlShema.validate(url);
-}
+  return urlShema.validate(url);
+};
 
 export default () => {
-    const elements = {
-        form: document.querySelector('form'),
-        inputEl: document.querySelector('#url-input'),
-        feedback: document.querySelector('.feedback'),
-        submitBtn: document.querySelector('button[type=submit]'),
-    };
+  const elements = {
+    form: document.querySelector('form'),
+    inputEl: document.querySelector('#url-input'),
+    feedback: document.querySelector('.feedback'),
+    submitBtn: document.querySelector('button[type=submit]'),
+  };
 
-    const state = {
-        form: {
-            formStatus: 'filling',
-            errors: '',
-        },
-        feeds: [],
-        posts: [],
-    };
+  const state = {
+    form: {
+      formStatus: 'filling',
+      errors: '',
+    },
+    feeds: [],
+    posts: [],
+  };
 
+  const watchedState = watcher(elements, state);
 
-    const watchedState = watcher(elements, state);
+  elements.form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    elements.form.addEventListener('submit', (e) => {
-        e.preventDefault();
+    const newData = new FormData(e.target);
+    const url = newData.get('url');
 
-        const newData = new FormData(e.target)
-        const url = newData.get('url');
-        
-        validate(url, watchedState.feeds)
-          .then((feed) => {
-            console.log(feed)
-            watchedState.form.formStatus = 'sent';
-            watchedState.form.errors = null;
-            state.feeds.push(feed);
-        })
-          .catch((err) => {
-            console.log(err.message)
-            watchedState.form.errors = err.message;
-            watchedState.form.formStatus = 'error';
-          })
-          console.log(state)
-          console.log(elements.feedback)
-    });
+    validate(url, watchedState.feeds)
+      .then((feed) => {
+        console.log(feed);
+        watchedState.form.formStatus = 'sent';
+        watchedState.form.errors = null;
+        state.feeds.push(feed);
+      })
+      .catch((err) => {
+        watchedState.form.errors = err.message;
+        watchedState.form.formStatus = 'error';
+      });
+  });
 };
--- src/view.js
+++ src/view.js
@@ -1,58 +1,62 @@
 import onChange from 'on-change';
 
 const handleProcess = (elements, state) => {
-    if (state.form.formStatus !== 'error') {
-        elements.inputEl.classList.remove('is-invalid');
-        elements.inputEl.focus();
-        elements.form.reset();
-    } else {
-        elements.inputEl.classList.add('is-invalid');
-    }
+  if (state.form.formStatus !== 'error') {
+    elements.inputEl.classList.remove('is-invalid');
+    elements.inputEl.focus();
+    elements.form.reset();
+  } else {
+    elements.inputEl.classList.add('is-invalid');
+  }
 };
 
 const renderFeedback = (elements, state) => {
-    elements.feedback.innerHTML = '';
+  const { feedback } = elements;
+  feedback.innerHTML = '';
 
-    switch (state.form.errors) {
-        case null: {
-            elements.feedback.innerHTML = 'RSS успешно загружен';
-            elements.feedback.classList.add('text-success');
-            elements.feedback.classList.remove('text-danger');
-            break;
-        }
-        case 'not uniq URL': {
-            elements.feedback.innerHTML = 'RSS уже существует';
-            elements.feedback.classList.remove('text-success');
-            elements.feedback.classList.add('text-danger');
-            break;
-        }
-        case 'invalid URL': {
-            elements.feedback.innerHTML = 'Ссылка должна быть валидным URL';
-            elements.feedback.classList.remove('text-success');
-            elements.feedback.classList.add('text-danger');
-            break;
-        }
-        default: 
-            break;
+  switch (state.form.errors) {
+    case null: {
+      feedback.textContent = 'RSS успешно загружен';
+      feedback.classList.add('text-success');
+      feedback.classList.remove('text-danger');
+      break;
     }
+    case 'not uniq URL': {
+      feedback.textContent = 'RSS уже существует';
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      break;
+    }
+    case 'invalid URL': {
+      feedback.textContent = 'Ссылка должна быть валидным URL';
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      break;
+    }
+    default:
+      break;
+  }
 };
 
 export default (elements, state) => {
-    const mainWatcher = onChange(state, (path, value) => {
-        switch (path) {
-            case 'feeds': {
-                state.form.feeds.push(value); //проверить на дубли и если нет, добавить in state.feeds. отрендерить контейнер фидов
-                break;
-            }
-            case 'form.formStatus': {
-                handleProcess(elements, state); // в зависимости от состояния менять цвет рамки инпута
-                break;
-            }
-            case 'form.errors': {
-                renderFeedback(elements, state); // рендер ошибки в контекст feedback
-                break;
-            }
-        }
-    });
-    return mainWatcher;
+  const mainWatcher = onChange(state, (path, value) => {
+    switch (path) {
+      case 'feeds': {
+        state.form.feeds.push(value);
+        // <-- проверить на дубли и если нет, добавить in state.feeds. отрендерить контейнер фидов
+        break;
+      }
+      case 'form.formStatus': {
+        handleProcess(elements, state); // в зависимости от состояния менять цвет рамки инпута
+        break;
+      }
+      case 'form.errors': {
+        renderFeedback(elements, state); // рендер ошибки в контекст feedback
+        break;
+      }
+      default:
+        break;
+    }
+  });
+  return mainWatcher;
 };
