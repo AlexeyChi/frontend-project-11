2fb44751b8528d46569b287da278845ab216b33c
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-01-31 00:33:34 +0300

    Added some keys

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -4,6 +4,11 @@ export default {
       null: 'RSS успешно загружен',
       notUniqURL: 'RSS уже существует',
       invalidURL: 'Ссылка должна быть валидным URL',
+      notContainValidRss: 'Ресурс не содержит валидный RSS',
+      networkError: 'Упс... что-то пошло не так, попробуйте повторить позже'
     },
+    feeds: 'Фиды',
+    posts: 'Посты',
+    buttonContext: 'Просмотр',
   },
 };
