abd06714a57330e62bb29a6af83f7196face9abc
AlexeyChi <chibiryaev.alexey@yandex.ru>
2024-01-31 00:31:55 +0300

    Created routes, parserResp(), makeNewData()

--- src/app.js
+++ src/app.js
@@ -1,14 +1,46 @@
 import * as yup from 'yup';
 import i18next from 'i18next';
+import axios from 'axios';
 import resources from './locales/index.js';
 import watcher from './view.js';
+import urlParser from './parsers/parser.js';
 
+let uniqId = 1;
 const validate = (url, feeds) => {
   const urlShema = yup
     .string()
     .url('invalid URL')
     .notOneOf(feeds, 'not uniq URL');
   return urlShema.validate(url, { abortEarly: false });
+};
+
+const routes = {
+  fetchUrl: (url) => `https://allorigins.hexlet.app/get?url=${encodeURIComponent(`${url}`)}`,
+};
+
+const makeParsedResponse = (data) => {
+  return axios.get(routes.fetchUrl(data))
+    .then((response) => urlParser(response.data.contents))
+    .catch((e) => {
+      if (e.message === 'Network error') {
+        throw new Error(`networkError`)
+      }
+      throw e;
+    });
+};
+
+const makeNewData = (data, url) => {
+  const { feed, posts } = data;
+  const { title, description } = feed;
+  const newFeed = { id: uniqId, link: url, ...{ title, description } };
+  uniqId += 1;
+  const newPosts = [...posts].map((post) => {
+    post.id = uniqId;
+    uniqId += 1;
+    post.feedId = newFeed.id;
+    return post;
+  });
+  return { newFeed, newPosts };
 };
 
 export default () => {
@@ -33,8 +65,10 @@ export default () => {
   const elements = {
     form: document.querySelector('form'),
     inputEl: document.querySelector('#url-input'),
-    feedback: document.querySelector('.feedback'),
+    feedbackEl: document.querySelector('.feedback'),
     submitBtn: document.querySelector('button[type=submit]'),
+    feedsContainer: document.querySelector('div.feeds'),
+    postsContainer: document.querySelector('div.posts'),
   };
 
   const state = {
@@ -53,16 +87,24 @@ export default () => {
 
     const newData = new FormData(e.target);
     const url = newData.get('url');
+    const uniqFeeds = state.feeds.map(({ link }) => link);
 
-    validate(url, watchedState.feeds)
-      .then((feed) => {
+    validate(url, uniqFeeds)
+      .then((url) => {
         watchedState.form.formStatus = 'sent';
+        return makeParsedResponse(url);
+      })
+      .then((data) => {
         watchedState.form.errors = null;
-        state.feeds.push(feed);
+        const { newFeed, newPosts } = makeNewData(data, url);
+        watchedState.feeds.push(newFeed);
+        watchedState.posts.push(...newPosts);
+        console.log(state)
       })
       .catch((err) => {
-        watchedState.form.errors = err.message;
         watchedState.form.formStatus = 'error';
+        watchedState.form.errors = err.message;
+        console.log(state)
       });
   });
 };
